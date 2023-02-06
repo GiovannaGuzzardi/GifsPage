5d2977aa7cffeeb370168509682846464838d2d2
giovanna guzzardi <giovannaguzzard@bkpi.io>
2023-02-06 14:42:54 -0300

    "atualização de funções sem responsibilidade"

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import './App.css';
+
 import React, { useState } from "react";
+import './App.css';
 import Img from './componentes/ListGif/ListGif';
 import LikeProvider from './context/curtidas';
-import pokeball from '../src/pokeball.png'
 
 function App() {
 
   const [query, setQuery] = useState("");
   const [gifs, setGifs] = useState("");
+  const [status, setStatus] = useState(false)
 
 
   const api = {
     key: "HqE5P3JKqV9FpldQJmbIIWW23vJg9z5s",
     acao: "search",
     busca: query,
-    limite: 6,
+    limite: 27,
   };
 
 
 
 
   const consumer = (evt) => {
-    fetch(`//api.giphy.com/v1/gifs/${api.acao}?api_key=${api.key}&q=${api.busca}&limit=${api.limite}&offset=0&rating=g&lang=pt`)
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result)
-        setGifs(result)
-        setQuery("")
-      })
+    if (status === false) {
+      document.getElementById("Input").focus()
+      setStatus(true)
+
+    }
+    else {
+      if (query === "") {
+        alert("insira um valor valido")
+        return
+      }
+      else {
+        fetch(`//api.giphy.com/v1/gifs/${api.acao}?api_key=${api.key}&q=${api.busca}&limit=${api.limite}&offset=0&rating=g&lang=pt`)
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result)
+            setGifs(result)
+            setQuery("")
+          })
+        setStatus(false)
+      }
+    }
   }
 
   return (
@@ -35,24 +50,33 @@ function App() {
       <div className="app">
         <main className="main">
           <div className='navBar'>
-            <div>
-              <img className='iconPoke' src={pokeball} alt="icone" />
-              <h1></h1>
+            <div className="flex">
+              <img className='iconPoke' src="/pokeball.png" alt="icone" />
+              <h1 className="titulo">PokeGif</h1>
             </div>
-            <div>
+            <div className="flex">
               <input
+                id="Input"
                 type="text"
                 onChange={(e) => setQuery(e.target.value)}
                 value={query}
                 placeholder="Pesquisa..."
+                className={status ? "Input" : "InputI"}
               />
-              <button onClick={consumer} type="button">Click Me!</button>
+              <button onClick={consumer} type="button" className="Button"> <i class="fa-solid fa-magnifying-glass Button"></i></button>
             </div>
           </div>
-          {gifs.data && (
+          {gifs.data ? (
             <div className='listGif'>
               <Img arr={gifs.data} />
             </div>)
+            :
+            <div className="inicial">
+              <img className="gifPoke" src="https://media3.giphy.com/media/42wQXwITfQbDGKqUP7/giphy-downsized.gif?cid=318e1caazy8qswujarimtxn0q694ej20sghuap5c4khb366t&amp;rid=giphy-downsized.gif&amp;ct=g" alt="animation film GIF by POKÉMON Detective Pikachu" />
+              <h2> Bem-vindo a este divertido mundo de gifs.</h2>
+              <h2> Pesquise o que você quiser</h2>
+
+            </div>
           }
         </main>
       </div >
